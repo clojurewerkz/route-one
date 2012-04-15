2d56ecebb61b40b548b82c46407e2425175c511d
Michael S. Klishin <michael@novemberain.com>
2012-04-15 20:07:11 +0400

    Clojure 1.4.0

--- project.clj
+++ project.clj
@@ -6,10 +6,10 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clojurewerkz/urly   "1.0.0-rc2"]]
   :source-paths ["src/clojure"]
-  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta6"]]}}
+  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
   :aliases { "all" ["with-profile" "dev:dev,1.4"] }
-  :repositories {"clojure-releases" "http://build.clojure.org/releases",
-                 "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases",
-                             :snapshots false,
-                             :releases {:checksum :fail, :update :always}}}
+  :repositories {"clojure-releases" "http://build.clojure.org/releases"
+                 "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}}
   :warn-on-reflection true)
