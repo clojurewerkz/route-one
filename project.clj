20849633d803d4e34981a1fb62d83380c97004fd
Michael S. Klishin <michael@novemberain.com>
2012-09-07 03:45:30 +0400

    Move to Clojure 1.4, add CI against 1.5-master-SNAPSHOT

--- project.clj
+++ project.clj
@@ -3,13 +3,16 @@
   :url "http://github.com/clojurewerkz/route-one"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [clojurewerkz/urly   "1.0.0-rc2"]]
   :source-paths ["src/clojure"]
-  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
-  :aliases { "all" ["with-profile" "dev:dev,1.4"] }
-  :repositories {"clojure-releases" "http://build.clojure.org/releases"
-                 "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,1.3:dev,1.5"]}
+  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
-                             :releases {:checksum :fail :update :always}}}
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}}
   :warn-on-reflection true)
