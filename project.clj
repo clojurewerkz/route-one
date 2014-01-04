b2911407d55d7f5617798e7aa5c53e1179694f46
Michael Klishin <michael@clojurewerkz.org>
2014-01-04 17:00:25 +0400

    Test against Clojure 1.4, 1.5 and 1.6 (master)

--- project.clj
+++ project.clj
@@ -6,10 +6,10 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clojurewerkz/urly   "2.0.0-alpha5"]]
   :source-paths ["src/clojure"]
-  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
+  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :dev {:dependencies [[compojure "1.1.5"]]}}
-  :aliases {"all" ["with-profile" "dev:dev,1.3:dev,1.5"]}
+  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.6"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail :update :always}}
