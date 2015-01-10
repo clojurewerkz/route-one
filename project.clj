9430d29c4cb9f0c0f2ec66c174b0d8d07610c5fc
Michael Klishin <michael@clojurewerkz.org>
2015-01-10 18:36:24 +0300

    Test against Clojure 1.7.0-alpha5

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
   :source-paths ["src/clojure"]
   :profiles {:dev {:dependencies [[compojure "1.1.8"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
   :aliases {"all" ["with-profile" "dev:dev,1.5:dev,1.7:dev,master"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
