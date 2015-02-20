5e29cb175b1f98a47efa1aab3230879a1c0b94a4
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2015-02-20 22:36:45 +0100

    First stab at compojure-like context feature

--- src/clojure/clojurewerkz/route_one/compojure.clj
+++ src/clojure/clojurewerkz/route_one/compojure.clj
@@ -2,41 +2,37 @@
   (:require [compojure.core :as compojure]
             [clojurewerkz.route-one.core :as route-one]))
 
+(def ^{:dynamic true} *route-prefix* nil)
+
+(defmacro handler-with-route [req-handler name path args body]
+  `(do
+     (let [route-path# (if *route-prefix* (str *route-prefix* ~path) ~path)]
+       (route-one/defroute ~(symbol (str name)) route-path#))
+     (~req-handler ~path ~args ~@body)))
+
 (defmacro GET "Generate a GET route."
   [name path args & body]
-  `(do
-     (route-one/defroute ~(symbol (str name)) ~path)
-     (compojure/GET ~path ~args ~@body)))
+  `(handler-with-route compojure.core/GET ~name ~path ~args ~body))
 
 (defmacro POST "Generate a POST route."
   [name path args & body]
-  `(do
-     (route-one/defroute ~(symbol (str name)) ~path)
-     (compojure/POST ~path ~args ~@body)))
+  `(handler-with-route compojure.core/POST ~name ~path ~args ~body))
 
 (defmacro PUT "Generate a PUT route."
   [name path args & body]
-  `(do
-     (route-one/defroute ~(symbol (str name)) ~path)
-     (compojure/PUT ~path ~args ~@body)))
+  `(handler-with-route compojure.core/PUT ~name ~path ~args ~body))
 
 (defmacro OPTIONS "Generate a OPTIONS route."
   [name path args & body]
-  `(do
-     (route-one/defroute ~(symbol (str name)) ~path)
-     (compojure/OPTIONS ~path ~args ~@body)))
+  `(handler-with-route compojure.core/OPTIONS ~name ~path ~args ~body))
 
 (defmacro HEAD "Generate a HEAD route."
   [name path args & body]
-  `(do
-     (route-one/defroute ~(symbol (str name)) ~path)
-     (compojure/HEAD ~path ~args ~@body)))
+  `(handler-with-route compojure.core/HEAD ~name ~path ~args ~body))
 
 (defmacro PATCH "Generate a PATCH route."
   [name path args & body]
-  `(do
-     (route-one/defroute ~(symbol (str name)) ~path)
-     (compojure/PATCH ~path ~args ~@body)))
+  `(handler-with-route compojure.core/PATCH ~name ~path ~args ~body))
 
 (defmacro ANY "Generate a ANY route."
   [name path args & body]
@@ -48,3 +44,13 @@
   "Generate a Catch-all fallback route"
   [args & body]
   `(compojure/ANY "*" ~args ~@body))
+
+(defmacro context [path args & routes]
+  `(let [path# (if *route-prefix* (str *route-prefix* ~path) ~path)]
+     (binding [*route-prefix* path#]
+       (let [routes# (compojure/routes ~@routes)]
+         (compojure/context ~path ~args routes#)))))
+
+(defmacro routes [& handlers]
+  `(fn []
+     (compojure/routes ~@handlers)))
--- test/clojurewerkz/route_one/compojure_test.clj
+++ test/clojurewerkz/route_one/compojure_test.clj
@@ -1,4 +1,5 @@
 (ns clojurewerkz.route-one.compojure-test
+  (:require [compojure.core :as compojure])
   (:use clojure.test
         clojurewerkz.route-one.compojure))
 
@@ -21,3 +22,16 @@
                        "res")]
       (is (= "res" (:body (handler
                            (fake-request :get (users-index-path) {:get :params}))))))))
+
+(deftest nested-routes-test
+  (testing "Handler in context"
+    ((context "/user/current" []
+               (GET user-profile "/profile" {:keys [params]} "res"))
+      (fake-request :get "/user/current/profile" {:get :params}))
+    (is (= (user-profile-path) "/user/current/profile")))
+  (testing "Handler in nested contexts"
+    (let [inner (routes (context "/tasks" []
+                          (GET user-add-task "/add" {:keys [params]} "res")))
+          outer (context "/user/current" [] (inner))]
+      (is (= "res" (:body (outer (fake-request :get "/user/current/tasks/add" {:get :params})))))
+      (is (= (user-add-task-path) "/user/current/tasks/add")))))
